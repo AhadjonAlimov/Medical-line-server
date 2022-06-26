4bbb5924b07a18ef16a5d8dee32ca21346448b79
Alimov <84062546+AhadjonAlimov@users.noreply.github.com>
2022-06-26 20:03:23 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const cors = require('cors');
+
+const authRoutes = require("./routes/auth.js");
+
+const PORT = process.env.PORT || 5000;
+const app = express();
+
+require('dotenv').config();
+
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded());
+
+app.get('/', (req, res) => {
+    res.send('Hello World');
+});
+
+app.use('/auth', authRoutes);
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
